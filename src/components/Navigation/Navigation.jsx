0e9f56fc8787252155e9dc28ad2faf11c13684dd
Vitaliy Gerassimov <102645214+VitaliyDeveloper@users.noreply.github.com>
2023-02-04 12:24:18 +0600

    3

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -63,35 +63,6 @@ const Navigation = () => {
           </NavListSubmenu>
         </NavItemTopMenu>
       </NavList>
-      {/* <NavList>
-        <NavItem>
-          <Link
-            href="https://github.com/VitaliyDeveloper"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link
-            href="https://www.linkedin.com/in/vitaliy-gerassimov-665255236/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link
-            href="mailto:gerassimov.developer@mail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            gerassimov.developer@gmail.com
-          </Link>
-        </NavItem>
-      </NavList> */}
       <Dot1></Dot1>
       <Dot2></Dot2>
     </NavigalionContainer>
